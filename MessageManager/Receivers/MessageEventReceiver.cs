293f6922a1a03a22c381598574bb4f3f4c63fc4c
Michael Schulz <schulz.95@osu.edu>
2018-02-03 11:43:17 -0800

    Added two types of MessageReceivers, an event based on (fire and forget) and a queue based one, which will require the consumer to ask for a message rather than wait for a message

---
+++ MessageManager/Receivers/MessageEventReceiver.cs
@@ -0,0 +1,27 @@
+﻿using MsgMgr.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsgMgr.Receivers
+{
+    public class EventBasedMessageReceiver : IMessageReceiver
+    {
+        /// <summary>
+        /// Occurs when [message received].
+        /// </summary>
+        public event MessageManagerMessageReceivedEventHandler MessageReceived;
+        
+        /// <summary>
+        /// Accepts a received message by raising [MessageReceived] event.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public void AcceptReceivedMessage(MessageBase message)
+        {
+            MessageReceived?.Invoke(new MessageManagerMessageReceivedEventArgs(message));
+        }
+        
+    }
+}
